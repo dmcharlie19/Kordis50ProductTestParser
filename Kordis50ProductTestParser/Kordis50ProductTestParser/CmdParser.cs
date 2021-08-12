bcd45dcc5873a8accd19ddd95e4f48809636db3d
dmcharlie19 <39765206+dmcharlie19@users.noreply.github.com>
2021-08-12 16:48:24 +0300

    Add files via upload

---
+++ Kordis50ProductTestParser/Kordis50ProductTestParser/CmdParser.cs
@@ -0,0 +1,64 @@
+﻿/*-----------------------------------------------------------------------------
+CmdParser.cs
+
+Класс для получения параметров теста из аргументов командной строки
+-----------------------------------------------------------------------------*/
+using System;
+
+namespace Kordis50ProductTestParser
+{
+    class CmdParser
+    {
+        public static CommandLineArgumentsParsingResult GetParameterUnsignedIntegerValueFromStringArgument(String argumentString, String parameterName, out uint parameter)
+        {
+            uint value = 0;
+            parameter = value;
+
+            if (argumentString.Length == 0 || parameterName.Length == 0)
+            {
+                return CommandLineArgumentsParsingResult.IncorrectSyntax;
+            }
+
+            if (!argumentString.Contains(parameterName))
+                return CommandLineArgumentsParsingResult.ArgumentNotFound;
+
+            var index = argumentString.IndexOf(parameterName) + parameterName.Length;
+            if (index < 0)
+                return CommandLineArgumentsParsingResult.IncorrectSyntax;
+
+            var parameterValueText = argumentString.Substring(index);
+            try
+            {
+                parameter = Convert.ToUInt32(parameterValueText);
+            }
+            catch (System.Exception)
+            {
+                return CommandLineArgumentsParsingResult.IncorrectSyntax;
+            }
+
+            return CommandLineArgumentsParsingResult.Success;
+        }
+
+        public static CommandLineArgumentsParsingResult GetStringArgument(String argumentString, String parameterName, out string parameter)
+        {
+            string value = "";
+            parameter = value;
+
+            if (argumentString.Length == 0 || parameterName.Length == 0)
+            {
+                return CommandLineArgumentsParsingResult.IncorrectSyntax;
+            }
+
+            if (!argumentString.Contains(parameterName))
+                return CommandLineArgumentsParsingResult.ArgumentNotFound;
+
+            var index = argumentString.IndexOf(parameterName) + parameterName.Length;
+            if (index < 0)
+                return CommandLineArgumentsParsingResult.IncorrectSyntax;
+
+            parameter = argumentString.Substring(index);
+
+            return CommandLineArgumentsParsingResult.Success;
+        }
+    }
+}
---
+++ Kordis50ProductTestParser/Kordis50ProductTestParser/FlashProgrammer.cs
@@ -0,0 +1,75 @@
+﻿/*-----------------------------------------------------------------------------
+FlashProgrammer.cs
+
+Класс для загрузки прошивки в память МК.
+Используется стороння программа nrfjprog.
+-----------------------------------------------------------------------------*/
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace Kordis50ProductTestParser
+{
+    class FlashProgrammer
+    {
+        public static int ProgrammFlash()
+        {
+            // 1. Очистка памяти
+            int res = ExecuteNrfjprogProcess("--eraseall");
+            if (res != 0)
+                return SystemParameters.RETURN_CODE_FAIL;
+
+            // 2. Загрузка прошивки в память
+            string localFilePath = Path.GetTempPath() + "tempHex.hex";
+            TestExecutionParameters parameters = TestParametersReader.GetTestExecutionParameters();
+
+            try
+            {
+                // Получение ссылки на Team Foundation Server. 
+                var tfs = new TeamFoundationServer(parameters.Server);
+
+                // Получение ссылки на Version Control. 
+                var versionControl = (VersionControlServer)tfs.GetService(typeof(VersionControlServer));
+                versionControl.DownloadFile(parameters.ServerPath, 0, VersionSpec.Latest, localFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"TFS error: {ex.Message}");
+                return SystemParameters.RETURN_CODE_FAIL;
+            }
+
+            string arg = "--program " + localFilePath;
+            res = ExecuteNrfjprogProcess(arg);
+
+            // Удаляем временный файл
+            File.Delete(localFilePath);
+
+            if (res != 0)
+                return SystemParameters.RETURN_CODE_FAIL;
+
+            // 3. Перезапуск
+            res = ExecuteNrfjprogProcess("--reset");
+            if (res != 0)
+                return SystemParameters.RETURN_CODE_FAIL;
+
+            return SystemParameters.RETURN_CODE_SUCCESS;
+        }
+
+        private static int ExecuteNrfjprogProcess(string arg )
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = SystemParameters.NRFJPROG_PATH;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = false;
+            process.StartInfo.Arguments = arg;
+
+            process.Start();
+            process.WaitForExit(20000);
+
+            return process.ExitCode;
+        }
+    }
+}
